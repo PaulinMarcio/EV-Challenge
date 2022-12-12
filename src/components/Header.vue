6954936a716669d8a6a1ab4c5ea6b0fecfa0d3b9
Márcio Paulin <118933351+PaulinMarcio@users.noreply.github.com>
2022-12-12 13:30:37 -0300

    feat: Header
    Header pronto

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,9 @@
 <template>
   <header>
-    <!-- TODO -->
+    <button>
+      <img src="../assets/close.svg" alt="">
+    </button>
+    <h1>CADASTRO DE USUARIO</h1>
   </header>
 </template>
 
@@ -10,4 +13,26 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+header{
+  background-color: #E02B57;
+  display: flex;
+  height: 2rem;
+  align-items: center;
+  justify-content: center;
+  color: white;
+}
+
+button{
+  position: absolute;
+  left: 1rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+}
+
+h1{
+  font-size: .8rem;
+}
+
+</style>
